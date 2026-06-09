831f8a99b63fd3f494b17fba32b07b3a495fc10f
matthael <mfponte2006@gmail.com>
2026-06-09 10:28:11 -0300

    Implementa tabela hash com encadeamento

--- src/hash.c
+++ src/hash.c
@@ -1,9 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "hash.h"
 
-unsigned int hash_function(const char *key) {
-    unsigned int hash = 0;
-    while (*key) {
-        hash = (hash * 31u) ^ (unsigned char)(*key++);
+TabelaHash* criarHash(int tamanho) {
+    TabelaHash *hash = malloc(sizeof(TabelaHash));
+
+    if (!hash) {
+        printf("Erro ao criar tabela hash.\n");
+        return NULL;
     }
+
+    hash->tamanho = tamanho;
+    hash->total_colisoes = 0;
+    hash->tabela = calloc(tamanho, sizeof(No*));
+
+    if (!hash->tabela) {
+        printf("Erro ao alocar tabela hash.\n");
+        free(hash);
+        return NULL;
+    }
+
     return hash;
+}
+
+int funcaoHash(int x, int m) {
+    if (x < 0) {
+        x = -x;
+    }
+
+    return (x * 31) % m;
+}
+
+void inserirHash(TabelaHash *hash, Produto produto) {
+    int indice = funcaoHash(produto.id, hash->tamanho);
+
+    if (hash->tabela[indice] != NULL) {
+        hash->total_colisoes++;
+    }
+
+    No *novo = malloc(sizeof(No));
+
+    if (!novo) {
+        printf("Erro ao inserir na tabela hash.\n");
+        return;
+    }
+
+    novo->produto = produto;
+    novo->proximo = hash->tabela[indice];
+    hash->tabela[indice] = novo;
+}
+
+Produto* buscarHash(TabelaHash *hash, int id) {
+    int indice = funcaoHash(id, hash->tamanho);
+
+    No *atual = hash->tabela[indice];
+
+    while (atual != NULL) {
+        if (atual->produto.id == id) {
+            return &atual->produto;
+        }
+
+        atual = atual->proximo;
+    }
+
+    return NULL;
+}
+
+void liberarHash(TabelaHash *hash) {
+    if (!hash) {
+        return;
+    }
+
+    for (int i = 0; i < hash->tamanho; i++) {
+        No *atual = hash->tabela[i];
+
+        while (atual != NULL) {
+            No *temp = atual;
+            atual = atual->proximo;
+            free(temp);
+        }
+    }
+
+    free(hash->tabela);
+    free(hash);
 }
--- src/hash.h
+++ src/hash.h
@@ -1,6 +1,23 @@
 #ifndef HASH_H
 #define HASH_H
 
-unsigned int hash_function(const char *key);
+#include "produto.h"
+
+typedef struct No {
+    Produto produto;
+    struct No *proximo;
+} No;
+
+typedef struct {
+    No **tabela;
+    int tamanho;
+    long total_colisoes;
+} TabelaHash;
+
+TabelaHash* criarHash(int tamanho);
+int funcaoHash(int x, int m);
+void inserirHash(TabelaHash *hash, Produto produto);
+Produto* buscarHash(TabelaHash *hash, int id);
+void liberarHash(TabelaHash *hash);
 
 #endif
